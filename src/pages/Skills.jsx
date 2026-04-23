262766b52da146abb276118143565ddad3e7f2d3
linscot10 <linscot10@gmail.com>
2026-04-23 12:11:18 +0300

    portfolio project

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {faReact,faHtml5,faCss3Alt,faVuejs,faTailwindCss,faJs,faNodeJs,faPython,faGitAlt,faLinux}  from '@fortawesome/free-brands-svg-icons'
+
+import {faDatabase} from  '@fortawesome/free-solid-svg-icons'
+
 
 const Skills = () => {
   return (
@@ -6,50 +12,50 @@ const Skills = () => {
     <div className=' '>
         <h1 className='text-3xl mx-4 pt-8 px-10'>Tech Stack</h1><hr className='  bg-gray-900  m-4 mx-12 mt-6 border-[1px] '/>
  <div>
-    <div className=' px-10 bg-red-200 mx-5'>
-        <h3  className='text-2xl p-4'>Front_end</h3>
-        <ul className='flex gap-4 bg-red-600 mx-6'>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>React.JS</li>
-            <li>Vue.JS</li>
-            <li>Tailwind CSS</li>
-            <li>Javascript</li>
-            <li>SQL</li>
+    <div className=' px-10  mx-5'>
+        <h3  className='text-2xl p-4 flex gap-1 justify-start items-end'> <p className='justify-center text-3xl text-blue-500 items-start'>|</p>Front_end</h3>
+        <ul className='flex gap-4  mx-6 justify-center'>
+            <li  className=' group  hover:bg-blue-200 hover:h-1/3   hover:rounded-md text-gray-600 hover:text-white w-1/3 text-center h-12 p-3 '><FontAwesomeIcon icon={faHtml5}  className='text-orange-400 text-3xl group-hover:text-5xl'/>HTML</li>
+            <li  className='group hover:bg-blue-200 hover:h-1/3  hover:rounded-md text-gray-600 hover:text-white w-1/3 text-center h-12 p-3'><FontAwesomeIcon icon={faCss3Alt}  className='text-blue-400 text-3xl group-hover:text-5xl'/>CSS</li>
+            <li  className='group hover:bg-blue-200 hover:h-1/3  hover:rounded-md text-gray-600 hover:text-white w-1/3 text-center h-12 p-3'><FontAwesomeIcon icon={faReact}  className='text-blue-400 text-3xl group-hover:text-5xl'/>React.JS</li>
+            <li  className='group hover:bg-blue-200 hover:h-1/3  hover:rounded-md text-gray-600 hover:text-white w-1/3 text-center h-12 p-3'><FontAwesomeIcon icon={faVuejs}  className='text-green-400 text-3xl group-hover:text-5xl'/>Vue.JS</li>
+            <li  className='group hover:bg-blue-200  hover:h-1/3 hover:rounded-md text-gray-600 hover:text-white w-1/3 text-center h-12 p-3'><FontAwesomeIcon icon={faTailwindCss}  className='text-blue-400 text-3xl group-hover:text-5xl'/>Tailwind</li>
+            <li  className='group hover:bg-blue-200 hover:h-1/3  hover:rounded-md text-gray-600 hover:text-white w-1/3 text-center h-12 p-3'><FontAwesomeIcon icon={faJs}  className='text-yellow-400 text-3xl group-hover:text-5xl'/>Javascript</li>
+            <li  className='group hover:bg-blue-200 hover:h-1/3  text-gray-600 hover:text-white w-1/3 text-center h-12 p-3 hover:rounded-md'><FontAwesomeIcon icon={faDatabase}  className='text-blue-400 text-3xl group-hover:text-5xl'/>SQL</li>
         </ul>
     </div>
-    <div className=' px-10 bg-red-200 mx-5'>
-        <h3  className='text-2xl p-4'>Back_end</h3>
-        <ul className='flex gap-4 bg-red-600 mx-6'>
-            <li>Node.JS</li>
-            <li>Express.js</li>
-            <li>Python</li>
-            <li>Django</li>
+    <div className=' px-10  mx-5'>
+        <h3  className='text-2xl p-4 flex gap-1 justify-start items-end'> <p className='justify-center text-3xl text-blue-500 items-start'>|</p><span className=''>Back_end</span></h3>
+        <ul className='flex gap-4  mx-6'>
+            <li className='group hover:bg-blue-200 hover:h-1/3 text-gray-600 hover:text-white w-1/3 text-center h-12 p-3 hover:rounded-md'><FontAwesomeIcon icon={faNodeJs}  className='text-green-400 text-3xl group-hover:text-5xl'/>Node.JS</li>
+            <li className='group hover:bg-blue-200 hover:h-1/3 text-gray-600 hover:text-white w-1/3 text-center h-12 p-3 hover:rounded-md'>Express.js</li>
+            <li className='group hover:bg-blue-200 hover:h-1/3 text-gray-600 hover:text-white w-1/3 text-center h-12 p-3 hover:rounded-md'><FontAwesomeIcon icon={faPython}  className='text-blue-400 text-3xl group-hover:text-5xl'/>Python</li>
+            <li className='group hover:bg-blue-200 hover:h-1/3 text-gray-600 hover:text-white w-1/3 text-center h-12 p-3 hover:rounded-md'>Django</li>
         </ul>
     </div>
-    <div className=' px-10 bg-red-200 mx-5'>
-        <h3  className='text-2xl p-4'>Data Analysis</h3>
-        <ul className='flex gap-4 bg-red-600 mx-6'>
-            <li>Numpy</li>
-            <li>Pandas</li>
-            <li>Matplotlib</li>
-            <li>Seaborn</li>
+    <div className=' px-10  mx-5'>
+        <h3  className='text-2xl p-4 flex gap-1 justify-start items-end'> <p className='justify-center text-3xl text-blue-500 items-start'>|</p><span className=''>Data  Analysis</span> </h3>
+        <ul className='flex gap-4  mx-6'>
+            <li className='group hover:bg-blue-200 hover:h-1/3 text-gray-600 hover:text-white w-1/3 text-center h-12 p-3 hover:rounded-md'>Numpy</li>
+            <li className='group hover:bg-blue-200 hover:h-1/3 text-gray-600 hover:text-white w-1/3 text-center h-12 p-3 hover:rounded-md'>Pandas</li>
+            <li className='group hover:bg-blue-200 hover:h-1/3 text-gray-600 hover:text-white w-1/3 text-center h-12 p-3 hover:rounded-md'>Matplotlib</li>
+            <li className='group hover:bg-blue-200 hover:h-1/3 text-gray-600 hover:text-white w-1/3 text-center h-12 p-3 hover:rounded-md'>Seaborn</li>
         </ul>
     </div>
-    <div className=' px-10 bg-red-200 mx-5'>
-        <h3  className='text-2xl p-4'>Tools</h3>
-        <ul className='flex gap-4 bg-red-600 mx-6'>
-            <li>Git</li>
-            <li>Vs Code</li>
-            <li>Postman</li>
-            <li>Postman</li>
+    <div className=' px-10  mx-5'>
+        <h3  className='text-2xl p-4 flex gap-1 justify-start items-end'> <p className='justify-center text-3xl text-blue-500 items-start'>|</p><span className=''> Tools</span></h3>
+        <ul className='flex gap-4  mx-6'>
+            <li className='group hover:bg-blue-200 hover:h-1/3 text-gray-600 hover:text-white w-1/3 text-center h-12 p-3 hover:rounded-md'><FontAwesomeIcon icon={faGitAlt}  className='text-orange-600 text-3xl group-hover:text-5xl'/>Git</li>
+            <li className='group hover:bg-blue-200 hover:h-1/3 text-gray-600 hover:text-white w-1/3 text-center h-12 p-3 hover:rounded-md'>Vs Code</li>
+            <li className='group hover:bg-blue-200 hover:h-1/3 text-gray-600 hover:text-white w-1/3 text-center h-12 p-3 hover:rounded-md'>Postman</li>
+            <li className='group hover:bg-blue-200 hover:h-1/3 text-gray-600 hover:text-white w-1/3 text-center h-12 p-3 hover:rounded-md'><FontAwesomeIcon icon={faLinux}  className='text-black-400 text-3xl group-hover:text-5xl'/>Linux</li>
         </ul>
     </div>
-    <div className=' px-10 bg-red-200 mx-5'>
-        <h3  className='text-2xl p-4'>Database</h3>
-        <ul className='flex gap-4 bg-red-600 mx-6'>
-            <li>Mysql</li>
-            <li>MongoDBB</li>
+    <div className=' px-10  mx-5'>
+        <h3  className='text-2xl p-4 flex gap-1 justify-start'> <p className='justify-center text-3xl text-blue-500 items-start'>|</p><span> Database</span></h3>
+        <ul className='flex gap-4  mx-6'>
+            <li className='group hover:bg-blue-200 hover:h-1/3 text-gray-600 hover:text-white w-1/3 text-center h-12 p-3 hover:rounded-md'>Mysql</li>
+            <li className='group hover:bg-blue-200 hover:h-1/3 text-gray-600 hover:text-white w-1/3 text-center h-12 p-3 hover:rounded-md'>MongoDBB</li>
             
         </ul>
     </div>
