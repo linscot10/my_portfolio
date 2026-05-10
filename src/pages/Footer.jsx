7d1f1bd3b0661a4e2c28b5f88dd108150d4ed253
linscot10 <linscot10@gmail.com>
2026-05-10 20:23:16 +0300

    portfolio project

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
-import { faGithub, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Footer = () => {
   return (
-    <div>
-      <hr/>
-      <div>
-      <div>
-      <h2>Osuru Lawrence Scott</h2>
-      <p>Software Engineer with a passion for creating innovative, scalable, and high-performance solutions.</p>
+    <div className='flex items-center justify-center flex-col py-5 px-3 my-5'>
+      <hr className='  bg-gray-900  m-4 mx-12 mt-6 border-[1px]  w-screen'/>
+      <div className='flex items-center justify-center gap-5  p-5 w-screen'>
+      
+      <div className='w-1/3 px-5'>
+      <h2 className='text-2xl my-4'>Osuru Lawrence Scott</h2>
+      <p className='text-gray-400'>Software Engineer with a passion for creating innovative, scalable, and high-performance solutions.</p>
+     
       </div>
-      <div>
-      <ul>
+      <div className='w-1/3 flex flex-col items-center justify-center'>
+      <h3 className='text-xl my-2'>Links</h3>
+      <ul className='flex flex-col items-center justify-center text-sm'>
         <li><a href="Home">Home</a></li>
         <li><a href="About">About</a></li>
         <li><a href="Skills">Skills</a></li>
@@ -21,20 +25,23 @@ const Footer = () => {
       </ul>
       </div>
 
-      <div>
-        <h3>Connect</h3>
-        <ul>
-          <li className=' text-gray-700  hover:text-black p-3 '><a href='#'><FontAwesomeIcon className='text-black w-8  h-8' icon={faGithub} /></a></li>
-                      <li className=' text-gray-700  hover:text-black pt-3'><a href='#'><FontAwesomeIcon className='text-blue-600 w-8 h-8' icon={faLinkedin} /></a></li>
-                      <li className=' text-gray-700  hover:text-black pt-3'><a href='#'><FontAwesomeIcon className='text-red-600 w-8 h-8' icon={faInstagram} /></a></li>
+      <div className='w-1/3 flex items-center justify-center gap-2 flex-col'>
+        <h3 className='text-xl my-2'>Connect</h3>
+        <ul  className='flex items-center justify-center gap-1'>
+          <li className=' text-gray-700   pt-2 '><a href='#'><FontAwesomeIcon className='text-black w-6  h-6' icon={faGithub} /></a></li>
+                      <li className=' text-gray-700  pt-2'><a href='#'><FontAwesomeIcon className='text-blue-600 w-6 h-6' icon={faLinkedin} /></a></li>
+                      <li className=' text-gray-700  pt-2'><a href='#'><FontAwesomeIcon icon={faEnvelope} className='text-black w-6 h-6' /></a></li>
         </ul>
       </div>
       <div>
       </div>
 
       </div>
-      <hr/>
-<p>&copy;2026 osuru lawrence. All rights reserved</p>
+      <hr className='  bg-gray-500  m-4  mt-6 border-[1px]  w-3/4'/>
+      <div className='text-sm'> 
+      <p>&copy;2026 osuru lawrence. All rights reserved</p>
+      </div>
+
     </div>
   )
 }
