9865df64b5b4c1891a1e53a0f07df3769127c9f9
linscot10 <linscot10@gmail.com>
2026-05-15 08:31:10 +0300

    portfolio project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image_1 from './../assets/vscode.png'
+import Portfolio from './../assets/Portfolio.png'
 
 const Projects = () => {
   return (
@@ -7,7 +8,7 @@ const Projects = () => {
         <h1 className=" text-4xl p-3 ">Projects</h1>
       <div className="flex justify-center items-center p-5 ">
         <div className="flex   bg-gray-300  flex-col p-3 py-2 m-2 rounded-lg hover:border-gray-400 hover:border-[2px] shadow-lg h-[460px]  w-[300px]">
-          <div className=" w-1/2 rounded-lg"><img src={Image_1} alt="images" className=" rounded-lg" /></div>
+          <div className=" w-3/4 rounded-lg"><img src={Portfolio} alt="images" className=" rounded-lg" /></div>
           <p className=' text-gray-400'><strong className='text-black'>Period:</strong> <span>April 2026-May2026</span></p>
 
           <h1  className="text-xl py-2">Portfolio Project</h1>
@@ -19,7 +20,7 @@ const Projects = () => {
           <p> Perfect for handling multiple languages seamlessly.</p>
           <div className='flex gap-2 py-2'><p className="bg-gray-500 rounded-lg px-2">React.js</p> <p className="bg-gray-500 rounded-lg px-2">Tailwind</p></div>
 
-          <a href="#" className='py-2' >View Project</a>
+          <a href="#" className='py-2 hover:underline ' >View Project</a>
         </div>
         <div className="flex   bg-gray-300  flex-col p-3 py-2 m-2 rounded-lg hover:border-gray-400 hover:border-[2px] shadow-lg h-[460px]  w-[300px]">
           <div className=" w-1/2 rounded-lg"><img src={Image_1} alt="images" className=" rounded-lg" /></div>
@@ -32,7 +33,7 @@ const Projects = () => {
           <p>Translate text or files up to 5000 characters, with automatic language detection and multi-threaded translation for efficiency.</p>
           <p> Perfect for handling multiple languages seamlessly.</p>
           <div className='flex gap-2 py-2'><p className="bg-gray-500 rounded-lg px-2">React.js</p> <p className="bg-gray-500 rounded-lg px-2">Tailwind</p></div>
-          <a href="# className='py-2'">View Project</a>
+          <a href="#" className='py-2 hover:underline'>View Project</a>
         </div>
         <div className="flex   bg-gray-300  flex-col p-3 py-2 m-2 rounded-lg hover:border-gray-400 hover:border-[2px] shadow-lg h-[460px]  w-[300px]">
           <div className=" rounded-lg"><img src={Image_1} alt="images" className=" rounded-lg" width='150' /></div>
@@ -45,7 +46,7 @@ const Projects = () => {
           <p>Translate text or files up to 5000 characters, with automatic language detection and multi-threaded translation for efficiency.</p>
           <p> Perfect for handling multiple languages seamlessly.</p>
           <div className='flex gap-2 py-2'><p className="bg-gray-500 rounded-lg px-2">React.js</p> <p className="bg-gray-500 rounded-lg px-2">Tailwind</p></div>
-          <a href="# className='py-2'">View Project</a>
+          <a href="#" className='py-2 hover:underline'>View Project</a>
         </div>
         <div className="flex   bg-gray-300  flex-col p-3 py-2 m-2 rounded-lg hover:border-gray-400 hover:border-[2px] shadow-lg h-[460px]  w-[300px]">
           <div className=" rounded-lg"><img src={Image_1} alt="images" className=" rounded-lg" width='150' /></div>
@@ -58,7 +59,7 @@ const Projects = () => {
           <p>Translate text or files up to 5000 characters, with automatic language detection and multi-threaded translation for efficiency.</p>
           <p> Perfect for handling multiple languages seamlessly.</p>
           <div className='flex gap-2 py-2'><p className="bg-gray-500 rounded-lg px-2">React.js</p> <p className="bg-gray-500 rounded-lg px-2">Tailwind</p></div>
-          <a href="# className='py-2'">View Project</a>
+          <a href="#" className='py-2 hover:underline'>View Project</a>
         </div>
       </div>
 
