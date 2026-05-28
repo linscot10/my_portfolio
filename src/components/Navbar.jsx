00ed91d39dfeecc9a5ca41495b259ed4591f21e3
linscot10 <linscot10@gmail.com>
2026-05-28 15:37:34 +0300

    portfolio project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, {useState} from 'react'
 import { faXmark, faBars } from '@fortawesome/free-solid-svg-icons'
-import {faPaperPlane} from  '@fortawesome/free-solid-svg-icons'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
@@ -8,7 +7,8 @@ const Navbar = () => {
   const [isOpen, setIsopen]= useState(false);
   return (
     <div className=' flex   mx-10 pt-5 '>
-        <nav className='max-w-7xl gap-10 px-4 flex   justify-between items-center h-16 '>
+   
+        <nav className='max-w-7xl gap-10 px-4 flex   justify-between items-center h-16  w-full'>
         
             <div className='md:text-3xl  font-bold' >{"</>"}LAW<span className='text-green-700'>RENCE</span></div>
             <ul className='hidden md:flex  gap-4 md:text-2xl  '>
@@ -18,6 +18,8 @@ const Navbar = () => {
             <li className='flex items-center hover:text-blue-500'><a href='#Projects'>Projects</a></li>
             <li className='flex items-center hover:text-blue-500'><a href='#Contact'>Contact</a></li>
             </ul>
+
+            
 <button
 onClick={()=>setIsopen(!isOpen)}
 className="md:hidden focus:outline-none"
@@ -33,17 +35,34 @@ className="md:hidden focus:outline-none"
 
 </button>
 
+ <ul className=' hidden md:flex flex-row gap-4 md:text-xl  ml-auto  items-center'>
+                 <li  className='flex items-center'><a href='#Home'>Blog</a></li>
+            <button className=' text-lg flex items-center justify-center rounded-md w-24 bg-white p-2 hover:bg-gray-200'>Theme</button>
+              </ul>
+
 
 
             
               
             
         </nav>
+ {
+          isOpen && (
+                       <ul className='md:hidden  pb-4 px-4 space-y-2 '>
+            <li className=' flex items-center hover:text-blue-500'><a href='#Home'>Home</a></li>
+            <li className='flex items-center hover:text-blue-500'><a href='#About'>About</a></li>
+            <li className='flex items-center hover:text-blue-500'><a href='#Skills'>Skills</a></li>
+            <li className='flex items-center hover:text-blue-500'><a href='#Projects'>Projects</a></li>
+            <li className='flex items-center hover:text-blue-500'><a href='#Contact'>Contact</a></li>
+              <li  className='flex pt-4 items-center'><a href='#Home'>Blog</a></li>
+            <button className='flex items-center rounded-md bg-white p-2 mr-2'>Theme</button>
+            </ul>
+            
+          )
+        }
+        
 
-        <ul className=' hidden md:flex flex-row gap-4 md:text-xl  ml-auto '>
-                 <li  className='flex items-center'><a href='#Home'>Blog</a></li>
-            <button className='flex items-center rounded-lg bg-white p-2 mr-2'>Theme</button>
-              </ul>
+       
     </div>
   )
 }
