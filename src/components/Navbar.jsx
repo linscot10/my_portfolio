55395b0302543581036da6cb230ae524b4e06833
linscot10 <linscot10@gmail.com>
2026-05-28 10:03:38 +0300

    portfolio project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,58 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Navbar = () => {
+  const [isOpen, setIsopen]= useState(false);
   return (
-    <div className=' flex  gap-7 mx-10 pt-5 '>
-        <nav className='flex flex-row gap-10  justify-start '>
+    <div className=' flex   mx-10 pt-5 '>
+        <nav className='max-w-7xl gap-10 px-4 flex   justify-between items-center h-16 '>
+        
             <div className='md:text-3xl  font-bold' >{"</>"}LAW<span className='text-green-700'>RENCE</span></div>
-            <ul className='flex flex-row gap-4 md:text-2xl  '>
-            <li className='flex items-center'><a href='#Home'>Home</a></li>
-            <li className='flex items-center'><a href='#About'>About</a></li>
-            <li className='flex items-center'><a href='#Skills'>Skills</a></li>
-            <li className='flex items-center'><a href='#Projects'>Projects</a></li>
-            <li className='flex items-center'><a href='#Contact'>Contact</a></li>
+            <ul className='hidden md:flex  gap-4 md:text-2xl  '>
+            <li className=' flex items-center hover:text-blue-500'><a href='#Home'>Home</a></li>
+            <li className='flex items-center hover:text-blue-500'><a href='#About'>About</a></li>
+            <li className='flex items-center hover:text-blue-500'><a href='#Skills'>Skills</a></li>
+            <li className='flex items-center hover:text-blue-500'><a href='#Projects'>Projects</a></li>
+            <li className='flex items-center hover:text-blue-500'><a href='#Contact'>Contact</a></li>
             </ul>
+<button
+onClick={()=>setIsopen(!isOpen)}
+className="md:hidden focus:outline-none"
+>
+{
+  isOpen?(
+    <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+  ):(
+     <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+  )
+
+}
+
+</button>
+
 
 
             
@@ -19,7 +60,7 @@ const Navbar = () => {
             
         </nav>
 
-        <ul className='flex flex-row gap-4 md:text-xl  ml-auto'>
+        <ul className=' hidden md:flex flex-row gap-4 md:text-xl  ml-auto '>
                  <li  className='flex items-center'><a href='#Home'>Blog</a></li>
             <button className='flex items-center rounded-lg bg-white p-2 mr-2'>Theme</button>
               </ul>
