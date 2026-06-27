fecf08db645d283f5686db3c9dbb77a30d799b9b
linscot10 <linscot10@gmail.com>
2026-06-27 07:31:42 +0300

    portfolio project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,26 +7,27 @@ import myResume from './../assets/osuru lawrence CV.pdf (1).pdf'
 
 const Home = () => {
   return (
-    <div className='bg-mint-100 mt-0 h-screen' id='Home'>
+    <div className=' bg-mint-100 mt-0 ' id='Home'>
 
       <Navbar />
-      <div className="flex md:flex-row mt-20  gap-20 justify-center mx-5  pb-10 pt-11 ">
-        <div className=' flex flex-col w-1/2 p-2  justify-center'>
-          <h1 className='md:text-5xl p-3'><strong>I'm  Lawrence</strong></h1>
-          <h2 className='md:text-3xl text-gray-700 p-3'>Software Developer</h2>
-          <p className=' text-gray-700 p-3'>Turning ideas into interactive reality</p>
+      <div className=" flex  md:flex-row  mt-3 md:mt-20 gap-8 md:gap-20 justify-center mx-5 pb-2 md:pb-10 pt-4 md:pt-11 overflow-hidden  items-center ">
+        <div className='  w-full flex flex-col md:w-1/2 p-2  justify-center'>
+          <h1 className=' text-xl md:text-5xl p-1 md:p-3'><strong>I'm  Lawrence</strong></h1>
+          <h2 className='text-lg md:text-3xl text-gray-700 p-1 md:p-3'>Software Developer</h2>
+          <p className=' text-gray-700 md:text-2xl text-sm p-1 md:p-3'>Turning ideas into interactive reality</p>
           <ul className='flex gap-2'>
-            <li className=' text-gray-700  hover:text-black p-3 '><a href='#'><FontAwesomeIcon className='text-black w-8  h-8' icon={faGithub} /></a></li>
-            <li className=' text-gray-700  hover:text-black pt-3'><a href='#'><FontAwesomeIcon className='text-blue-600 w-8 h-8' icon={faLinkedin} /></a></li>
-            <li className=' text-gray-700  hover:text-black pt-3'><a href='#'><FontAwesomeIcon className='text-red-600 w-8 h-8' icon={faInstagram} /></a></li>
+            <li className=' text-gray-700  hover:text-black  p-1 md:p-3'><a href='#'><FontAwesomeIcon className='text-black md:w-8  md:h-8 w-4  h-4' icon={faGithub} /></a></li>
+            <li className=' text-gray-700  hover:text-black pt-1 p-1 md:pt-3'><a href='#'><FontAwesomeIcon className='text-blue-600 md:w-8 md:h-8' icon={faLinkedin} /></a></li>
+            <li className=' text-gray-700  hover:text-black pt-1 p-1 md:pt-3'><a href='#'><FontAwesomeIcon className='text-red-600 md:w-8 md:h-8' icon={faInstagram} /></a></li>
           </ul>
 
-          <a href={myResume} download='resume.pdf'><button className='bg-green-400 w-32 h-12 hover:bg-green-600 rounded-md'>Download CV</button>
-       </a> 
-          </div>
+          <a href={myResume} download='resume.pdf'><button className='bg-green-400 w-32 md:w-32 md:h-12 h-8 text-xl my-3 hover:bg-green-600 rounded-md'>Download CV</button>
+          </a> 
+        </div>
 
-        <div className='flex justify-start  mx-15 w-1/2  '>
-          <img className='text-xl ' src={image} width='400' />
+        <div className='flex justify-start  mx-auto md:w-1/2  '>
+          <img className='text-xl '  alt="Developer illustration"
+   className="w-72 md:w-96" src={image}/>
         </div>
       </div>
 
